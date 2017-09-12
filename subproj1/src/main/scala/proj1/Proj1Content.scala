a28096e4f545918a4f7434a133672f0565ec765e
Tama (Masanori Fujta) <fujita@nulab-inc.com>
2017-09-12 16:52:15 +0900

    ブランチテスト

--- subproj1/src/main/scala/proj1/Proj1Content.scala
+++ subproj1/src/main/scala/proj1/Proj1Content.scala
@@ -1,12 +1,12 @@
 package proj1
 
-import org.apache.commons.codec.Decoder
+import org.apache.commons.codec.{Decoder, Encoder}
 import org.apache.commons.codec.binary.Base64
 
 object Proj1Content {
   val message = "This is sub project 1."
-  def test():Unit = {
-    val d:Decoder = new Base64()
-    println(d.decode(message))
+  def test():String = {
+    val d:Encoder = new Base64()
+    d.encode(message).toString
   }
 }
