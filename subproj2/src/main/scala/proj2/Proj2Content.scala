ec98801ecc3d671612acd58b5d1096cbcbbf3bf5
Tama (Masanori Fujta) <fujita@nulab-inc.com>
2017-09-12 16:46:11 +0900

    returns http get response

--- subproj2/src/main/scala/proj2/Proj2Content.scala
+++ subproj2/src/main/scala/proj2/Proj2Content.scala
@@ -1,16 +1,15 @@
 package proj2
 
-import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClientBuilder
 
 object Proj2Content {
   val message = "This is sub project 2."
-  def test():Unit = {
+  def test():String = {
     val c = HttpClientBuilder.create().build()
     val get = new HttpGet("https://www.nulab-inc.com")
     val response = c.execute(get)
-    println(response.toString)
+    "get https://www.nulab-inc.com:  \n" + response.toString
 
   }
 }
